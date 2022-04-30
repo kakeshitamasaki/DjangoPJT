da22ce5b7c83641915b4303677007d796c4d359d
kake337 <kakeshitamasaki@gmail.com>
2022-05-01 01:42:06 +0900

    11

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,9 +6,13 @@ app_name = "myapp"  # NameSpaseの仕組み
 urlpatterns = [
     #path('admin/', admin.site.urls),
     path("hello/", views.index), # views.def名
-    path("products/",views.products),
+    path("products/",views.products,name="products"),
     path("products/<int:id>/",views.product_detail,name="product_detail"),  # urlに書いてあるidによって変化させる
     path("products/Test/",views.Test),
+    path("products/add/",views.add_product,name="add_product"),
+    path("products/update/<int:id>/",views.update_product,name="update_product"),  # urlに書いてあるid(数字)によって変化させる
+    path("products/delete/<int:id>/",views.delete_product,name="delete_product"),
+
 
 ]
 
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,5 @@
 from multiprocessing import context
-from django.shortcuts import render
+from django.shortcuts import render,redirect #ページ遷移用
 from django.http import HttpResponse #Requestを受けてResponseを返すため
 from .models import Product  # model Product Classを呼び出す
 from .models import Test_DB  # model Test_DB Classを呼び出す
@@ -23,6 +23,52 @@ def product_detail(request,id): # urlのid(数字をparameterとして)受け取
     }
     return render(request,"myapp/detail.html",context)
     #HttpResponse("The product id is:" + str(id)) テスト用
+
+def add_product(request):
+    if request.method == "POST":  # requestがpostだったらsubmitされたデータを取得する
+        name = request.POST.get("name")
+        price = request.POST.get("price")
+        desc = request.POST.get("desc")
+        image = request.FILES["upload"]
+
+        #データベースへ値を追加する
+        product = Product(name=name,price=price,desc=desc,image=image)
+        product.save()
+    return render(request, "myapp/addproduct.html")
+
+def update_product(request,id):  # idはあくまで引数(Urlから取得する)
+    product = Product.objects.get(id=id)#urlの数字から、modelのidを検索
+    if request.method=="POST":
+        product.name = request.POST.get("name")
+        product.price = request.POST.get("price")
+        product.desc = request.POST.get("desc")
+        product.image = request.FILES["upload"]
+        product.save()
+        
+        return redirect("/myapp/products")
+
+
+    context={
+        "product":product
+    }
+    #HttpResponse("The product id is:" + str(id)) テスト用
+    return render(request, "myapp/updateproduct.html",context)
+
+
+def delete_product(request,id):
+    product = Product.objects.get(id=id)
+    context={
+        "product":product
+    }
+
+    if request.method =="POST":
+        product.delete()
+        return redirect("/myapp/products")
+    
+    return render(request,"myapp/delete.html",context)
+
+
+
 
 
 def Test(request):
