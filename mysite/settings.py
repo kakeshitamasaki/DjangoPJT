6478341626be0e1c7c291cadeb6f0d722457c33e
kake337 <kakeshitamasaki@gmail.com>
2022-05-07 01:07:46 +0900

    25

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@+5+gzm8i=)q5b&c8+97!vpll1zj93m!51zy7pskf-8s_m8fu&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["polar-hollows-53585.herokuapp.com","kake654321.pythonanywhere.com","kake4321.pythonanywhere.com","127.0.0.1"]  # 接続許可するホスト名
 
@@ -123,6 +124,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 STATICFILES_DIRS = (os.path.join(BASE_DIR,"myapp/static"),)
+print(STATICFILES_DIRS)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
